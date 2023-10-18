71d65bcc80f95c8c98475458437b7746b10c21d3
Michal <micad132132@gmail.com>
2023-10-18 21:37:04 +0200

    tamagui added

--- magazineApp/App.tsx
+++ magazineApp/App.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TamaguiProvider, Button } from 'tamagui';
+import { useFonts } from 'expo-font';
+import config from './tamagui.config';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,9 +14,26 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      // can hide splash screen here
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <TamaguiProvider config={config}>
+      <View style={styles.container}>
+        <Button onLongPress={() => console.log('SIEMA')}>Hello world</Button>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </TamaguiProvider>
   );
 }
